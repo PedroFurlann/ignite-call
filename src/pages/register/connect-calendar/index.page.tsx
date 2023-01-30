9a8628a348b6f89e2b9511e5835ae001f9ed7204
Pedro Furlan <pedrofurlan6@gmail.com>
2023-01-30 19:17:57 -0300

    Create connect-calendar page

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,37 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Header, RegisterContainer } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function Register() {
+  // async function handleRegister(data: RegisterFormData) {}
+
+  return (
+    <RegisterContainer>
+      <Header>
+        <Heading as="strong">Conecte sua agenda</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as datas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit">
+          Próximo Passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </RegisterContainer>
+  )
+}
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -49,6 +49,8 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (error) {
       if (error instanceof AxiosError && error?.response?.data?.message) {
         alert(error.response.data.message)
