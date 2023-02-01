a700d1127e12c60c029cd0ad54e7c971f679b429
Pedro Furlan <pedrofurlan6@gmail.com>
2023-02-01 17:11:23 -0300

    Update Intervals

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,18 @@
+import { prisma } from '@/src/lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
+import { z } from 'zod'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,10 +29,23 @@ export default async function handler(
   )
 
   if (!session) {
-    return res.status(401)
+    return res.status(401).end()
   }
 
-  return res.json({
-    session,
-  })
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          timer_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }
